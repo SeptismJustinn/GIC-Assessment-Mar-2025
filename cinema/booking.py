ffd41cb09eff4c0481473ee6fa90574746133b6b
Tan Justinn <ohhoehoe@gmail.com>
2025-03-28 12:20:44 +0800

    Add Bookings class to manage Booking
    Bookings class wraps around the bookings dictionary, to have a more focused region in the code to control booking ID

--- cinema/booking.py
+++ cinema/booking.py
@@ -1,4 +1,20 @@
 class Booking:
-  def __init__(self, id, seats=[]):
+  def __init__(self, id, seats=[], confirmed=False):
     self.id = id
     self.seats = seats
+    self.confirmed = confirmed
+
+class Bookings:
+  def __init___(self, bookings: dict={}):
+    # Dictionary of Booking objects. Keys are the Booking.id
+    self.bookings = bookings
+
+  def create_booking(self, selection: list[tuple[int, int]]=[]) -> Booking:
+    booking_id = f"GIC{len(self.bookings.values() + 1):04d}"
+    new_booking = Booking(booking_id, selection)
+    # If there are other avenues to create booking, should add validation to ensure uniqueness
+    self.bookings[booking_id] = new_booking
+    return new_booking
+  
+  def get_booking(self, booking_id, fallback=None):
+    return self.bookings.get(booking_id, fallback)
