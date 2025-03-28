03e0d67899bbc3a5d01765b514e79b82b5e7acf5
Tan Justinn <ohhoehoe@gmail.com>
2025-03-28 15:05:24 +0800

    Add methods to create/update booking
    Corrected typo in Bookings __init__ method
    Corrected Booking.seats typing, maintain a dictionary instead of list

--- cinema/booking.py
+++ cinema/booking.py
@@ -1,20 +1,44 @@
 class Booking:
-  def __init__(self, id, seats=[], confirmed=False):
-    self.id = id
-    self.seats = seats
-    self.confirmed = confirmed
+  def __init__(self, id, count, seats={}, confirmed=False):
+    """
+    seats are dictionaries with the following shape:
+    {
+      row_idx: [List of seat_idx]
+    }
+
+    Row and seat are 0-indexed and refer to position in theatre matrix.
+    """
+    self.id: str = id
+    self.count: int = count
+    self.seats: dict = seats
+    self.confirmed: bool = confirmed
 
 class Bookings:
-  def __init___(self, bookings: dict={}):
-    # Dictionary of Booking objects. Keys are the Booking.id
-    self.bookings = bookings
+  def __init__(self, bookings={}):
+    """
+    bookings are dictionaries with the following shape:
+    {
+      booking_id: Booking object
+    }
+    """
+    self.bookings: dict = bookings
 
-  def create_booking(self, selection: list[tuple[int, int]]=[]) -> Booking:
-    booking_id = f"GIC{len(self.bookings.values() + 1):04d}"
-    new_booking = Booking(booking_id, selection)
+  def create_booking(self, tickets: int=0, selection: dict={}) -> Booking:
+    booking_id = f"GIC{(len(self.bookings.values()) + 1):04d}"
+    new_booking = Booking(booking_id, tickets, selection)
     # If there are other avenues to create booking, should add validation to ensure uniqueness
     self.bookings[booking_id] = new_booking
     return new_booking
+  
+  def update_booking(self, booking_id, selection: dict) -> Booking:
+    booking = self.get_booking(booking_id)
+    if not booking:
+      # Within the scope of assessment, update_booking is only called for unconfirmed bookings when changing seats,
+      # as such, they would have been verified prior to calling this method.
+      raise Exception("Booking not found!")
+    # Change seats dictionary reference
+    booking.seats = selection
+    return booking
   
   def get_booking(self, booking_id, fallback=None):
     return self.bookings.get(booking_id, fallback)
